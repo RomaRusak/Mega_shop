b0994a31a5d38fac36c27b7e6f7425a4ac19773e
Rusak Raman <romarusakr96@gmail.com>
2025-04-13 14:54:50 +0300

    add basic migrations

---
+++ database/migrations/2025_04_13_102407_create_discounts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('discount_percent', 5, 2);
+            $table->timestamp('discount_start');
+            $table->timestamp('discount_end');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discounts');
+    }
+};
