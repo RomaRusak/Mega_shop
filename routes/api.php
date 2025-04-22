3f3861f3139fbf97f74c4159562b6c94122c488d
Rusak Raman <romarusakr96@gmail.com>
2025-04-22 11:37:07 +0300

    add api.php

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\FiltersController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+
+Route::prefix('api/products')->group(function () {
+    Route::get('{category?}', [ProductsController::class, 'index'])
+        ->where('category', '[a-zA-Z\-_\']+')
+        ->name('products.index');
+
+    Route::get('{category?}/{id}', [ProductsController::class, 'show'])
+        ->where('category', '[a-zA-Z\-_\']+')
+        ->where('id', '[0-9]+')
+        ->name('products.show');
+
+    Route::get('{id}', [ProductsController::class, 'show'])
+        ->where('id', '[0-9]+')
+        ->name('products.show');
+});
+
+Route::post('/filters', [FiltersController::class, 'index'])->name('filters.index');
+    //    ->withoutMiddleware(VerifyCsrfToken::class);
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductsController;
-use App\Http\Controllers\FiltersController;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+
+require_once base_path('routes/api.php');
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::prefix('products')->group(function () {
-    Route::get('{category?}', [ProductsController::class, 'index'])
-        ->where('category', '[a-zA-Z\-_\']+')
-        ->name('products.index');
-
-    Route::get('{category?}/{id}', [ProductsController::class, 'show'])
-        ->where('category', '[a-zA-Z\-_\']+')
-        ->where('id', '[0-9]+')
-        ->name('products.show');
-
-    Route::get('{id}', [ProductsController::class, 'show'])
-        ->where('id', '[0-9]+')
-        ->name('products.show');
-});
-
-Route::post('/filters', [FiltersController::class, 'index'])->name('filters.index');
-    //    ->withoutMiddleware(VerifyCsrfToken::class);
