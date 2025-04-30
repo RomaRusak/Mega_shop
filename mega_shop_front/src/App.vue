86f27f69278a4193878452a3eb8adc1a9914ac82
Rusak Raman <romarusakr96@gmail.com>
2025-04-30 13:55:22 +0300

    integrate vuex

--- mega_shop_front/src/App.vue
+++ mega_shop_front/src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{JSON.stringify(getProducts)}}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import store from './store';
+import { mapGetters } from 'vuex';
+import { mapActions } from 'vuex';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  store,
+  data() {
+    return {
+      test: 'test',
+    }
+  },
+  computed: {
+    ...mapGetters(['getProducts']),
+  },
+  methods: {
+    ...mapActions(['asyncFetchProductsData']),
+  },
+  mounted() {
+    this.asyncFetchProductsData()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ mega_shop_front/src/store/modules/products.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+export default {
+    state() {
+        return {
+          productsData: {
+            products: [],
+            pagination: {},
+          },
+        };
+      },
+      mutations: {
+        SET_PRODUCTS_DATA(state, {products, pagination}) {
+            state.productsData = {
+                products: [...products],
+                pagination: {...pagination},
+            };
+        }
+      },
+      actions: {
+        async asyncFetchProductsData({commit}) {
+            try {
+                const responce = await axios.get("http://mega_shop.com/api/products");
+
+                if (responce.status === 200) {
+                    const {products, pagination} = responce.data.data;
+
+                    commit('SET_PRODUCTS_DATA', {products, pagination});
+                }
+            } catch(error) {
+                console.error(error);
+            }
+        }
+      },
+      getters: {
+        getProducts(state) {
+            console.log(state.productsData.products);
+            return state.productsData.products;
+        }
+      }
+};
